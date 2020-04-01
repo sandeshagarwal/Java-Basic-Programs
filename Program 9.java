7a360b491623e542da67b68ba75dd1e3514d5e25
sandeshagarwal <sandeshagarwal10@gmail.com>
2020-04-01 23:14:24 +0530

    P9 - Bitwise Operators in Java
    &,^,|,~

---
+++ Program 9.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main{
+  public static void main(String args[])
+  {
+    int a =5;
+    int b =7;
+    
+    System.out.println("a & b = "+(a&b));//AND
+    System.out.println("a | b = "+(a|b));//OR
+    System.out.println("a ^ b = "+(a^b));//XOR
+  System.out.println("~ a = "+(~a));//COMPLEMENT
+    }
+}
+    
